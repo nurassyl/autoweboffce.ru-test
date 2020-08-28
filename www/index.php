133328419c871f228b3cb3c364dce4a79c2089c2
Nurasyl Aldan <nurassyl.aldan@gmail.com>
2020-08-28 19:57:54 +0500

    Frontend initialized.

--- www/index.php
+++ www/index.php
@@ -8,3 +8,11 @@
 
 require_once(__DIR__ . '/../src/db.php');
 
+
+$title = 'Title';
+ob_start();
+include(__DIR__ . '/../src/views/index.phtml');
+$content = ob_get_clean();
+
+include(__DIR__ . '/../src/views/layout/index.phtml');
+
